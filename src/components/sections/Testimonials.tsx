b841bb52474d4a6e1108769cf238cfbe590fb6ca
oyewosolomon <oyewosolomon@gmail.com>
2025-02-28 18:44:51 +0100

    swiper navigation error corrected

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -96,8 +96,8 @@ const Testimonials: React.FC = () => {
         dynamicBullets: true, // Makes the pagination bullets more interactive
       },
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
       },
       breakpoints: {
         640: {
@@ -159,14 +159,14 @@ const Testimonials: React.FC = () => {
         <div className="mb-14 flex flex-col justify-center items-center sm:flex-row sm:items-center sm:justify-between max-sm:gap-8">
           <h2 className="text-4xl text-center font-bold text-white dark:text-white lg:text-center">Testimonials</h2>
           <div className="flex items-center gap-8">
-            <div className="swiper-button-prev custom-prev">
+            <div className="swiper-prev custom-prev">
               <div className="group flex justify-center items-center border border-solid border-white w-12 h-12 transition-all duration-500 rounded-full hover:bg-indigo-600">
                 <svg className="h-6 w-6 text-white group-hover:text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20.9999 12L4.99992 12M9.99992 6L4.70703 11.2929C4.3737 11.6262 4.20703 11.7929 4.20703 12C4.20703 12.2071 4.3737 12.3738 4.70703 12.7071L9.99992 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </div>
             </div>
-            <div className="swiper-button-next custom-next">
+            <div className="swiper-next custom-next">
               <div className="group flex justify-center items-center border border-solid border-white w-12 h-12 transition-all duration-500 rounded-full hover:bg-indigo-600">
                 <svg className="h-6 w-6 text-white group-hover:text-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M3 12L19 12M14 18L19.2929 12.7071C19.6262 12.3738 19.7929 12.2071 19.7929 12C19.7929 11.7929 19.6262 11.6262 19.2929 11.2929L14 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
